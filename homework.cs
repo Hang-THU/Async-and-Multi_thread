f73a7c068634c12127568ea1ca08bd962263bf04
lch24 <lch24tsinghua@qq.com>
2025-08-05 10:47:05 +0800

    Update homework.cs

--- homework.cs
+++ homework.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Threading;
 using System.Threading.Tasks;
@@ -32,6 +32,24 @@ public class Database
 
         // TODO: 使用 lock 语句保证线程安全
         // 提示：在 lock 块中查找书籍并更新库存，若库存不足则输出提示
+        lock (_lock)
+        {
+            foreach (Book book in _books)
+            {
+                if (book.Title == title)
+                {
+                    if (book.Inventory >= quantity)
+                    {
+                        book.Inventory -= quantity;
+                        Console.WriteLine($"成功购买{title}，数量{quantity}");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"库存不足，无法购买{title}");
+                    }
+                }
+            }
+        }
     }
 }
 
@@ -42,6 +60,7 @@ public class BookStore
     // TODO: 实现异步购书方法CheckoutAsync，调用 UpdateInventoryAsync
     public async Task CheckoutAsync(string bookTitle, int quantity)
     {
+        await _db.UpdateInventoryAsync(bookTitle, quantity);
     }
 
     public async Task SimulateMultipleUsers()
@@ -59,9 +78,14 @@ public class BookStore
         // 提示：创建多个 Task 调用 CheckoutAsync，并传入不同书名和数量
         var tasks = new List<Task>
         {
-            
+            CheckoutAsync("C#入门", 2),
+            CheckoutAsync("C#入门", 3),
+            CheckoutAsync("异步编程", 1),
+            CheckoutAsync("异步编程", 2),
+            CheckoutAsync("异步编程", 3),
         };
-
+        await Task.WhenAll(tasks);
+        Console.WriteLine("\n所有购书任务完成");
 
         Console.WriteLine("\n购买后库存：");
         books = await _db.GetBooksAsync();
