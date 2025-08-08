d90191aa4b3048368f44460b217c59174d1dfff6
lvhy24 <lvhy23@mails.tsinghua.edu.cn>
2025-08-09 07:52:11 +0800

    Update homework.cs

--- homework.cs
+++ homework.cs
@@ -1,15 +1,13 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Threading;
 using System.Threading.Tasks;
-
 public class Book
 {
     public string Title { get; set; }
     public decimal Price { get; set; }
     public int Inventory { get; set; }
 }
-
 public class Database
 {
     private readonly List<Book> _books = new List<Book>();
@@ -26,24 +24,39 @@ public class Database
         return Task.FromResult(_books);
     }
 
-    public async Task UpdateInventoryAsync(string title, int quantity)
+    public async Task UpdateInventoryAsync(string title, int quantity)//更新书籍和库存信息，购买quantity本书
     {
         await Task.Delay(100); // 模拟网络延迟
-
-        // TODO: 使用 lock 语句保证线程安全
-        // 提示：在 lock 块中查找书籍并更新库存，若库存不足则输出提示
+        lock (_lock)
+        {
+            var book = _books.Find(b => b.Title == title);
+            if (book != null)
+            {
+                if (book.Inventory >= quantity)
+                {
+                    book.Inventory -= quantity;
+                    Console.WriteLine($"成功购买 {quantity} 本《{book.Title}》，剩余库存：{book.Inventory}");
+                }
+                else
+                {
+                    Console.WriteLine($"库存不足，无法购买《{book.Title}》");
+                }
+            }
+            else
+            {
+                Console.WriteLine($"未找到书籍《{title}》");
+            }
+        }
     }
 }
-
 public class BookStore
 {
     private readonly Database _db = new Database();
-
-    // TODO: 实现异步购书方法CheckoutAsync，调用 UpdateInventoryAsync
     public async Task CheckoutAsync(string bookTitle, int quantity)
     {
+        Console.WriteLine($"正在购买《{bookTitle}》 {quantity} 本...");
+        await _db.UpdateInventoryAsync(bookTitle, quantity);
     }
-
     public async Task SimulateMultipleUsers()
     {
         var books = await _db.GetBooksAsync();
@@ -52,17 +65,15 @@ public class BookStore
         {
             Console.WriteLine($"- {book.Title}：{book.Inventory} 本");
         }
-
         Console.WriteLine("\n 开始模拟多用户购书...\n");
-
-        // TODO: 使用 Task.WhenAll 模拟多个用户并发购书
-        // 提示：创建多个 Task 调用 CheckoutAsync，并传入不同书名和数量
-        var tasks = new List<Task>
-        {
-            
-        };
-
-
+        Task t1 = CheckoutAsync("C#入门", 2);
+        Task t2 = CheckoutAsync("C#入门", 3);
+        Task t3 = CheckoutAsync("异步编程", 1);
+        Task t4 = CheckoutAsync("异步编程", 2);
+        Task t5 = CheckoutAsync("异步编程", 3);
+        //创建多个 Task 调用 CheckoutAsync，并传入不同书名和数量
+        var tasks = new List<Task> { t1, t2, t3, t4, t5 };
+        await Task.WhenAll(tasks);
         Console.WriteLine("\n购买后库存：");
         books = await _db.GetBooksAsync();
         foreach (var book in books)
@@ -70,9 +81,7 @@ public class BookStore
             Console.WriteLine($"- {book.Title}：{book.Inventory} 本");
         }
     }
-}
-
-public class Program
+}public class Program
 {
     public static async Task Main()
     {
